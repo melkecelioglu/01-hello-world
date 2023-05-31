e7ccab89b0f9ce713abfb55953a268d64307d83b
melkecelioglu <melkecelioglu@gmail.com>
2023-05-31 21:08:05 +0300

    [ADD]-transaction that fails

--- src/main.ts
+++ src/main.ts
@@ -33,10 +33,6 @@ await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
 const num0 = zkAppInstance.num.get();
 console.log('state after init:', num0.toString());
 // ----------------------------------------------------
-console.log('Shutting down');
-await shutdown();
-console.log('state after init:', num0.toString());
-// ----------------------------------------------------
 const txn1 = await Mina.transaction(senderAccount, () => {
   zkAppInstance.update(Field(9));
 });
@@ -44,6 +40,35 @@ await txn1.prove();
 await txn1.sign([senderKey]).send();
 const num1 = zkAppInstance.num.get();
 console.log('state after txn1:', num1.toString());
+// ----------------------------------------------------
+console.log('Shutting down');
+await shutdown();
+console.log('state after txn1:', num1.toString());
+// ----------------------------------------------------
+try {
+  const txn2 = await Mina.transaction(senderAccount, () => {
+    zkAppInstance.update(Field(75));
+  });
+  await txn2.prove();
+  await txn2.sign([senderKey]).send();
+} catch (ex: any) {
+  console.log(ex.message);
+}
+const num2 = zkAppInstance.num.get();
+console.log('state after txn2:', num2.toString());
+// ----------------------------------------------------
+console.log('Shutting down');
+await shutdown();
+
+console.log('state after txn2:', num2.toString());
+// ----------------------------------------------------
+const txn3 = await Mina.transaction(senderAccount, () => {
+  zkAppInstance.update(Field(81));
+});
+await txn3.prove();
+await txn3.sign([senderKey]).send();
+const num3 = zkAppInstance.num.get();
+console.log('state after txn3:', num3.toString());
 // ----------------------------------------------------
 console.log('Shutting down');
 await shutdown();

d9884d43301ece896fc7a0ce36065d612f261d8e
melkecelioglu <melkecelioglu@gmail.com>
2023-05-31 21:04:50 +0300

    [ADD]

---
+++ src/main.ts
@@ -0,0 +1,49 @@
+import { Square } from './Square.js';
+import {
+  isReady,
+  shutdown,
+  Field,
+  Mina,
+  PrivateKey,
+  AccountUpdate,
+} from 'snarkyjs';
+
+await isReady;
+console.log('SnarkyJS loaded');
+const useProof = false;
+const Local = Mina.LocalBlockchain({ proofsEnabled: useProof });
+Mina.setActiveInstance(Local);
+const { privateKey: deployerKey, publicKey: deployerAccount } =
+  Local.testAccounts[0];
+const { privateKey: senderKey, publicKey: senderAccount } =
+  Local.testAccounts[1];
+console.log('Shutting down');
+await shutdown(); // ----------------------------------------------------
+// Create a public/private key pair. The public key is your address and where you deploy the zkApp to
+const zkAppPrivateKey = PrivateKey.random();
+const zkAppAddress = zkAppPrivateKey.toPublicKey();
+// create an instance of Square - and deploy it to zkAppAddress
+const zkAppInstance = new Square(zkAppAddress);
+const deployTxn = await Mina.transaction(deployerAccount, () => {
+  AccountUpdate.fundNewAccount(deployerAccount);
+  zkAppInstance.deploy();
+});
+await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
+// get the initial state of Square after deployment
+const num0 = zkAppInstance.num.get();
+console.log('state after init:', num0.toString());
+// ----------------------------------------------------
+console.log('Shutting down');
+await shutdown();
+console.log('state after init:', num0.toString());
+// ----------------------------------------------------
+const txn1 = await Mina.transaction(senderAccount, () => {
+  zkAppInstance.update(Field(9));
+});
+await txn1.prove();
+await txn1.sign([senderKey]).send();
+const num1 = zkAppInstance.num.get();
+console.log('state after txn1:', num1.toString());
+// ----------------------------------------------------
+console.log('Shutting down');
+await shutdown();
